950f0dda0f46a1bc8076cc11b89e8ae3d8d67381
ga63zoc <maximilian.stadler@tum.de>
2018-08-11 17:44:37 +0200

    adapted scripts for pytroch detectron version

---
+++ dataset_conversion/shell/link_dataset_coco_detectron_pytorch.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#####################################
+# create sym-links of datasets into
+# $Detectron/detectron/datasets/data
+# $1 is path to Detectron
+# $2 is path to datasets
+
+while [ $# -gt 0 ]; do
+    case $1 in
+    -detectron_path)
+        detectron_path=$2
+        shift
+        ;;
+    -dataset_path)
+        dataset_path=$2
+        shift
+        ;;
+    *)
+        echo "Invalid argument: $1"
+        exit 1
+    esac
+    shift
+done
+
+if [ -d "${dataset_path}/coco" ]; then
+    echo "Linking coco dataset....";
+    
+    if [ ! -d ${detectron_path}/data/datasets/coco ]; then
+        mkdir -p ${detectron_path}/data/datasets/coco;
+    fi
+    
+    ln -s ${dataset_path}/coco/coco_test2014 ${detectron_path}/data/datasets/coco/;
+    ln -s ${dataset_path}/coco/coco_train2014 ${detectron_path}/data/datasets/coco/;
+    ln -s ${dataset_path}/coco/coco_val2014 ${detectron_path}/data/datasets/coco/;
+    ln -s ${dataset_path}/coco/annotations ${detectron_path}/data/datasets/coco/annotations;
+fi
+
---
+++ dataset_conversion/shell/link_dataset_tt100k_detectron_pytorch.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#####################################
+# create sym-links of datasets into
+# $Detectron/detectron/datasets/data
+# $1 is path to Detectron
+# $2 is path to datasets
+
+while [ $# -gt 0 ]; do
+    case $1 in
+    -detectron_path)
+        detectron_path=$2
+        shift
+        ;;
+    -dataset_path)
+        dataset_path=$2
+        shift
+        ;;
+    *)
+        echo "Invalid argument: $1"
+        exit 1
+    esac
+    shift
+done
+
+if [ -d "${dataset_path}/tt100k" ]; then
+    echo "Linking tt100k dataset....";
+    
+    if [ ! -d ${detectron_path}/data/datasets/tt100k ]; then
+        mkdir -p ${detectron_path}/data/datasets/tt100k;
+    fi
+    
+    ln -s $dataset_path/tt100k/data/test $detectron_path/data/datasets/tt100k/test;
+    ln -s $dataset_path/tt100k/data/train $detectron_path/data/datasets/tt100k/train;
+    ln -s $dataset_path/tt100k/data/other $detectron_path/data/datasets/tt100k/other;
+    ln -s $dataset_path/tt100k/data/JsonAnnotations $detectron_path/data/datasets/tt100k/annotations;
+fi
+
